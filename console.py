5fc5fe3c0e7f10050956068060a83a00535c232f
fabo893 <jose.rosa893@gmail.com>
2021-02-16 17:42:53 +0000

    show console

--- console.py
+++ console.py
@@ -5,6 +5,8 @@
 
 
 import cmd
+import json
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
 
 
@@ -13,12 +15,15 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
+
     """Commands"""
     def do_EOF(self, args):
         return True
 
+
     def do_quit(self, args):
         return True
+
 
     def do_create(self, args):
         if len(args) == 0:
@@ -29,36 +34,55 @@ class HBNBCommand(cmd.Cmd):
             new = BaseModel()
             print(new.id)
 
-    def do_show(self, *args):
-        print(len(args))
-        """
-        if len(args) == 1:
+
+    def do_show(self, info):
+        if len(info) == 0:
             print("** class name missing **")
             return
-        if args[1] != "BaseModel":
+        x = info.split(' ')
+        if x[0] != "BaseModel":
             print("** class doesn't exist **")
             return
-        if len(args) <= 2:
+        elif len(x) == 1:
             print("** instance id missing **")
             return
-        instance = args[1]
-        if instance.id != args[2]:
-            print("** no instance found **")
-            return
-        print(instance.__str__)"""
+        storage = FileStorage()
+        for key in storage.__objects:
+            print(key)
+            s = key.split('.')
+            if s[1] == x[1]:
+                print(s[0])
+                print(storage.__objects[key])
+                obj = s[0](**storage.__objects[key])
+                print(obj.__str__)
+                return
+
+        """
+        with open("file.json", 'r') as myfile:
+            obj_dict = json.load(myfile)
+            for key in obj_dict.keys():
+                s = key.split('.')
+                if s[1] == x[1]:
+                    print(key.__str__)
+                    return
+            print("** no instance found **")"""
 
     """Help documentation"""
     def help_EOF(self):
         print ("End-of-file command to exit the program\n")
 
+
     def help_quit(self):
         print ("Quit command to exit the program\n")
+
 
     def help_create(self):
         print("Create command to create a new instane of BaseModel\n")
 
+
     def help_show(self):
         print("Show command to show the string representation of instance\n")
+
 
     def emptyline(self):
         if self.lastcmd:
