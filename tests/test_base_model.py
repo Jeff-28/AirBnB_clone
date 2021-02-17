d3b0f2f531d0e827077f05454c626db021deaa7c
fabo893 <jose.rosa893@gmail.com>
2021-02-17 13:35:05 +0000

    changing the name of the file

---
+++ tests/test_base_model.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""
+Test Module - contains tests for the BaseModel class
+"""
+
+from models.base_model import BaseModel
+import unittest
+
+
+class TestBaseModel(unittest.TestCase):
+    """Defines the class tests"""
+
+
