3017d01073ee1641236812353a1ccdf1f014d610
fabo893 <jose.rosa893@gmail.com>
2021-02-17 00:58:13 +0000

    adding and fixing new commands

--- console.py
+++ console.py
@@ -8,7 +8,13 @@ import cmd
 import json
 from models import storage
 from models.base_model import BaseModel
+from models.city import City
+from models.state import State
+from models.amenity import Amenity
+from models.place import Place
+from models.review import Review
 
+cls_dict = {"BaseModel": BaseModel, "City": City, "State": State, "Amenity": Amenity, "Place": Place, "Review": Review}
 
 class HBNBCommand(cmd.Cmd):
     """Class for the console"""
@@ -28,10 +34,10 @@ class HBNBCommand(cmd.Cmd):
     def do_create(self, args):
         if len(args) == 0:
             print("** class name missing **")
-        elif args != "BaseModel":
+        elif args not in cls_dict:
             print("** class doesn't exist **")
         else:
-            new = BaseModel()
+            new = cls_dict[args]()
             print(new.id)
 
 
@@ -40,7 +46,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         x = info.split(' ')
-        if x[0] != "BaseModel":
+        if x[0] not in cls_dict.keys():
             print("** class doesn't exist **")
             return
         elif len(x) == 1:
@@ -50,10 +56,9 @@ class HBNBCommand(cmd.Cmd):
         all_objs = storage.all()
         for obj_id in all_objs.keys():
             if (obj_id == k):
-                obj = all_objs[obj_id]
-                print(obj)
+                print(all_objs[obj_id])
                 return
-        print("** instance no found **")
+        print("** no instance found **")
 
 
     def do_destroy(self, info):
@@ -61,7 +66,7 @@ class HBNBCommand(cmd.Cmd):
             print("** class name missing **")
             return
         x = info.split(' ')
-        if x[0] != "BaseModel":
+        if x[0] not in cls_dict.keys():
             print("** class doesn't exist **")
             return
         elif len(x) == 1:
@@ -75,7 +80,64 @@ class HBNBCommand(cmd.Cmd):
                 storage.__objects = all_objs
                 storage.save()
                 return
-            print("** instance no found **")
+        print("** no instance found **")
+
+
+    def do_all(self, arg):
+        x = []
+        if len(arg) == 0:
+            all_objs = storage.all()
+            for key in all_objs.keys():
+                obj = all_objs[key]
+                x.append(str(obj))
+            print(x)
+            return
+        else:
+            a = arg.split()
+            if a[0] in cls_dict:
+                all_objs = storage.all()
+                for key in all_objs.keys():
+                    obj = all_objs[key]
+                    k = key.split('.')
+                    if k[0] == a[0]:
+                        x.append(str(obj))
+                print(x)
+            else:
+                print("** class doesn't exist **")
+
+
+    def do_update(self, arg):
+        if len(arg) == 0:
+            print("** class name missing **")
+            return
+        args = arg.split(' ')
+        if args[0] in cls_dict.keys():
+            if len(args) >= 2:
+                k = "{:s}.{:s}".format(args[0], args[1])
+                all_objs = storage.all()
+                for key in all_objs.keys():
+                    if key == k:
+                        if len(args) >= 3:
+                            if len(args) >= 4:
+                                obj = all_objs[k]
+                                a = args[2]
+                                v = args[3]
+                                setattr(obj, a, v)
+                                return
+                            else:
+                                print("** value missing **")
+                                return
+                        else:
+                            print("** attribute name missing **")
+                            return
+                print("** no instance found **")
+                return
+            else:
+                print("** instance id missing **")
+                return
+        else:
+            print("** class doesn't exist **")
+            return
 
 
     """Help documentation"""
@@ -94,8 +156,17 @@ class HBNBCommand(cmd.Cmd):
     def help_show(self):
         print("Show command to show the string representation of instance\n")
 
+
     def help_destroy(self):
         print("Delete an instance\n")
+
+
+    def help_all(self):
+        print("Prints all string representation of all instances based or not on the class name\n")
+
+
+    def help_update(self):
+        print("Updates an instance based on the class name and id by adding or updating attribute\n")
 
 
     def emptyline(self):
