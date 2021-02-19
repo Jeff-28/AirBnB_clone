09c54f7c7f27c8862415cf0689287e10c384f1ba
Jeff Martinez <2408@holbertonschool.com>
2021-02-19 03:33:52 +0000

    testing inheritance

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,6 +5,7 @@ Test Module - contains tests for the User class
 
 import unittest
 from models.user import User
+from models.base_model import BaseModel
 
 
 class TestUser(unittest.TestCase):
@@ -19,11 +20,15 @@ class TestUser(unittest.TestCase):
 
     def test_new_instance(self):
         """Tests the creation of a new instance"""
-        user = User()
         self.assertIs(type(self.user1.email), str)
         self.assertIs(type(self.user1.password), str)
         self.assertIs(type(self.user1.first_name), str)
         self.assertIs(type(self.user1.last_name), str)
+
+    def test_inheritance(self):
+        """Tests that user inherits from BaseModel"""
+        user = User()
+        self.assertIsInstance(user, BaseModel)
         self.assertTrue(hasattr(user, "id"))
         self.assertTrue(hasattr(user, "created_at"))
         self.assertTrue(hasattr(user, "updated_at"))
